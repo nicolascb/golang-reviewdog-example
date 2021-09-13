d21a7baec749d868675983f4f942e8eee20c495e
Nicolas Barbosa <ndevbarbosa@gmail.com>
2021-09-13 13:17:16 -0300

    feat: example feature

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/nicolascb/golang-reviewdog-example/app"
+)
 
 func main() {
 	fmt.Println("vim-go")
+
+	a := app.NewApp(os.Getenv("VERSION"))
+	fmt.Println(a.Version())
 }
